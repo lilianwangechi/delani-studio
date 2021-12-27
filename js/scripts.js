060979615645406ce4d67e30fd9497477f4714cc
Lilianwangechi <lilian.wangechi@student.moringaschool.com>
2021-12-27 03:30:34 +0300

    add bottom margins to the cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,45 @@
 $(document).ready(function(){
-$(#one).click(function() {
-  $("img").hide();
+  $("#img_1").click(function(){
+    // $("#content11").css("bold","center");
+    $("#img_1").toggle();
+      $(".one").toggle();
+       $(".one").click(function(){
+        $(".one").hide();
+        $("#img_1").show();
+       });
+
+  });
 });
-$("#one").click(function(){
-$("p").show();
-})
+
+$(document).ready(function(){
+  $("#img_2").click(function(){
+    // $("#content11").css("bold","center");
+    $("#img_2").toggle();
+      $(".two").toggle();
+       $(".two").click(function(){
+        $(".two").hide();
+        $("#img_2").show();
+       });
+
+  });
+});
+
+$(document).ready(function(){
+  $("#img_3").click(function(){
+    // $("#content11").css("bold","center");
+    $("#img_3").toggle();
+      $(".three").toggle();
+       $(".three").click(function(){
+        $(".three").hide();
+        $("#img_3").show();
+       });
+
+  });
+});
 
 
-})
+// $(document).ready(function(){
+//   $("#img_1").click(function()})
+//     $(".one").show();
+//   }
+// });
