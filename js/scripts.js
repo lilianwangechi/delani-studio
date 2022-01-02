d846f7bacb47371ccdad25595474dedb1df86095
Lilianwangechi <lilian.wangechi@student.moringaschool.com>
2022-01-02 21:40:37 +0300

    hover effect using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// use interface
 $(document).ready(function(){
   $("#img_1").click(function(){
     // $("#content11").css("bold","center");
@@ -7,10 +8,8 @@ $(document).ready(function(){
         $(".one").hide();
         $("#img_1").show();
        });
-
   });
 });
-
 $(document).ready(function(){
   $("#img_2").click(function(){
     // $("#content11").css("bold","center");
@@ -20,10 +19,8 @@ $(document).ready(function(){
         $(".two").hide();
         $("#img_2").show();
        });
-
   });
 });
-
 $(document).ready(function(){
   $("#img_3").click(function(){
     // $("#content11").css("bold","center");
@@ -33,13 +30,62 @@ $(document).ready(function(){
         $(".three").hide();
         $("#img_3").show();
        });
-
   });
 });
+
+
+//hoover effect
+$(function(){
+  $(".space").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.4"}, 350);
+    $(".hove").show();
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 350);
+  });
+})
+
+
+
+
+
+// $('#work').mouseover(function () {
+//   $('#overlay').show();
+// }).mouseout(function () {
+//   $('#overlay').hide();
+// });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // $(document).ready(function(){
 //   $("#img_1").click(function()})
 //     $(".one").show();
 //   }
+// });
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   $("p").hover(function(){
+//     $(this).addClass("highlight");
+    
+//   },function(){
+//     $(this).removeClass("highlight");
+//   });
 // });
