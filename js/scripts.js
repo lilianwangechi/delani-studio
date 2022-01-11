7e031c093bb611ff92a2b455bd75b8fea8a27c3b
kamutu <wangechililian@gmail.com>
2022-01-11 04:38:12 +0300

    hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -34,16 +34,104 @@ $(document).ready(function(){
 //hoover effect
 
 $(function(){
-  $(".space").hover(
+  $(".space1").hover(
   function() {
     $(this).stop().animate({"opacity": "0.5"},350 );
-    $(".hove").show();   
+    $(".hove1").show();   
   },
   function() {
     $(this).stop().animate({"opacity": "1"}, 400);
-    $(".hove").hide();
+    $(".hove1").hide();
   });
 })
+
+$(function(){
+  $(".space2").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove2").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove2").hide();
+  });
+})
+
+$(function(){
+  $(".space3").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove3").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove3").hide();
+  });
+})
+
+$(function(){
+  $(".space4").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove4").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove4").hide();
+  });
+})
+
+$(function(){
+  $(".space5").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove5").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove5").hide();
+  });
+})
+
+$(function(){
+  $(".space6").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove6").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove6").hide();
+  });
+})
+
+$(function(){
+  $(".space7").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove7").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove7").hide();
+  });
+})
+
+$(function(){
+  $(".space8").hover(
+  function() {
+    $(this).stop().animate({"opacity": "0.5"},350 );
+    $(".hove8").show();   
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, 400);
+    $(".hove8").hide();
+  });
+})
+
+
+
+
 
 
 // business interface
